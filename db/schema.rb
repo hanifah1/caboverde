cf6ca87e7e1cbafe967776837c2807f63268ed71
hanifah1 <khanhanifah@gmail.com>
2018-05-09 00:42:07 -0400

    4.2 add price field and change from table to grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505200958) do
+ActiveRecord::Schema.define(version: 20180509040827) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180505200958) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
+    t.string "price"
   end
 
 end
