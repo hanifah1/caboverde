9653b76faa5de65511b357f916eb5e7905fde6f8
hanifah1 <khanhanifah@gmail.com>
2018-05-13 04:21:17 -0400

    4.5 database update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: 'seed-generated product', description: 'I also created this product without using the HTML form!')
+Product.create(name: 'The Flying Peanut', description: 'description', price: 23, image_url: 'pbutter5.jpg' )
+Product.create(name: 'Underwater Peanut', description: 'description', price: 44, image_url: 'pbutter3.jpg' )
+Product.create(name: 'Peanut Smarts', description: 'description', price: 37, image_url: 'pbutter2.jpg' )
+Product.create(name: 'PB Time Freeze', description: 'description', price: 46, image_url: 'pbutter8.jpg' )
+Product.create(name: 'Peanut Night Vision', description: 'description', price: 29, image_url: 'pbutter7.jpg' )
+Product.create(name: 'PB Animal Echolocation', description: 'description', price: 37, image_url: 'pbutter9.jpg' )
