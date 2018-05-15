3ccacd88dfb550e2be729be67aa2d51a3beac991
hanifah1 <khanhanifah@gmail.com>
2018-05-15 01:33:53 -0400

    4.6 Orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,6 @@ Product.create(name: 'Peanut Smarts', description: 'description', price: 37, ima
 Product.create(name: 'PB Time Freeze', description: 'description', price: 46, image_url: 'pbutter8.jpg' )
 Product.create(name: 'Peanut Night Vision', description: 'description', price: 29, image_url: 'pbutter7.jpg' )
 Product.create(name: 'PB Animal Echolocation', description: 'description', price: 37, image_url: 'pbutter9.jpg' )
+
+Order.create(user_id: 1, product_id: 1, total: 10.0)
+Order.create(user_id: 1, product_id: 2, total: 5.0)
