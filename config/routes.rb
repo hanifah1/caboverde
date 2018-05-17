72da7dabdbe24c6721d24f2a113dbb55e9d2e5b5
hanifah1 <khanhanifah@gmail.com>
2018-05-17 03:09:01 -0400

    5.2 forms

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
 
   get 'simple_pages/landing_page'
-  
-  root 'simple_pages#about'
+
+  root 'simple_pages#landing_page'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
