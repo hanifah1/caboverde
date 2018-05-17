686add859f9b145554ecbd7d9f9d00824601660e
hanifah1 <khanhanifah@gmail.com>
2018-05-17 15:02:25 -0400

    5.2 production search fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.development?
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+    elsif Rails.env.production?
+      @products = Product.where("name ilike ?", "%#{search_term}%")
+
+    end
   end
-end
