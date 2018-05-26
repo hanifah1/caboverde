02e891a7bdfdc1609ac206e87473da03b962b38b
hanifah1 <khanhanifah@gmail.com>
2018-05-26 00:48:32 -0400

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.delete_all
+Product.delete_all
+Order.delete_all
+
 Product.create(name: 'The Flying Peanut', description: 'description', price: 23, image_url: 'pbutter5.jpg' )
 Product.create(name: 'Underwater Peanut', description: 'description', price: 44, image_url: 'pbutter3.jpg' )
 Product.create(name: 'Peanut Smarts', description: 'description', price: 37, image_url: 'pbutter2.jpg' )
 Product.create(name: 'PB Time Freeze', description: 'description', price: 46, image_url: 'pbutter8.jpg' )
 Product.create(name: 'Peanut Night Vision', description: 'description', price: 29, image_url: 'pbutter7.jpg' )
 Product.create(name: 'PB Animal Echolocation', description: 'description', price: 37, image_url: 'pbutter9.jpg' )
-
-Order.create(user_id: 1, product_id: 2, total: 10.0)
-Order.create(user_id: 1, product_id: 5, total: 5.0)
