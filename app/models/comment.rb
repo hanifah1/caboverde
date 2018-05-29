974f30284d16eec98544874b81da056ed240c5fb
hanifah1 <khanhanifah@gmail.com>
2018-05-29 02:16:23 -0400

    5.8 comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to product_path(@product)
+  end
 
   scope :rating_desc, -> { order(rating: :desc) }
-  
+
 end
