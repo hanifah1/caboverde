2d732eb924ee991740bdb9595278133dbcff39c6
hanifah1 <khanhanifah@gmail.com>
2018-06-04 17:13:23 -0400

    6.2 average_rating not working

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,10 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
   def self.search(search_term)
     if Rails.env.development?
       @products = Product.where('name LIKE ?', "%#{search_term}%")
@@ -19,9 +23,5 @@ class Product < ApplicationRecord
   def highest_rating_comment
     comments.rating_desc.first
   end
-
-  def average_rating
-  comments.average(:rating).to_f
-end
 
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Product do
+  context "when the product has comments" do
+    let(:product) { Product.create!(name: "Blue Peanut Butter") }
+    let(:user) { User.create!(email: "user4@gmail.com", password: "userfour4")}
+
+    before do
+      product.comments.create!(rating: 1, user: user, body: "Awful PB!")
+      product.comments.create!(rating: 3, user: user, body: "Ok PB!")
+      product.comments.create!(rating: 5, user: user, body: "Great PB!")
+    end
+
+    it "returns the average rating of all comments" do
+      expect(product.average_rating).to eq 3
+    end
+  end
+
+end
