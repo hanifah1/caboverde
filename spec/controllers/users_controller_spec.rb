9c7d876204f71951556412af651c91cc2ab328f5
hanifah1 <khanhanifah@gmail.com>
2018-06-07 21:49:22 -0400

    6.4 added factories

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:user) { User.create!(email: "user4@gmail.com", password: "userfour4")}
-  let(:user2) { User.create!(email: "user5@gmail.com", password: "userfive5")}
+  before do
+    @user = FactoryBot.create(:user)
+  end
 
   describe 'GET #show' do
 
@@ -10,20 +11,20 @@ describe UsersController, type: :controller do
      context 'when a user is logged in' do
 
        before do
-         sign_in user
+         sign_in @user
        end
 
        it "loads correct user details" do
-         get :show, params: { id: user.id }
+         get :show, params: { id: @user.id }
          expect(response).to be_ok
-         expect(assigns(:user)).to eq user
+         expect(assigns(:user)).to eq @user
        end
 
      end
 
      context 'when a user is not logged in' do
        it 'redirects to homepage' do
-         get :show, params: { id: user.id }
+         get :show, params: { id: @user.id }
          expect(response).to redirect_to(root_path)
        end
      end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+
+    FactoryBot.define do
+      sequence(:email) { |n| "user#{n}@example.com" }
+    end
+    
+    email
+    password "1234567890"
+    first_name "Peter"
+    last_name "Example"
+    admin false
+  end
+end
