aa816aa2689684dd073fc673f5701b84a11820ee
hanifah1 <khanhanifah@gmail.com>
2018-06-12 00:12:10 -0400

    6.6 create orders

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-
+  resources :payments, only: [:create]
 
   get 'simple_pages/about'
 
