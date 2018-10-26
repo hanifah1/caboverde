e0d5b43a71f41805c8591594db48bc3e89f6646e
hanifah1 <khanhanifah@gmail.com>
2018-10-25 23:50:56 -0400

    CSS updates

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module Myapp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.assets.initialize_on_precompile = false
 
     config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
